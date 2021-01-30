99c369f2a7976069aeb3ab4412415e3a853db8d4
June <danpa725@cau.ac.kr>
2021-01-30 23:29:07 +0900

    Setup file directory AND Make basic routes components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
+import { Route } from "react-router-dom";
+
+import LandingPage from "./routes/LandingPage";
+import LoginPage from "./routes/LoginPage";
+import Register from "./routes/Register";
+
 function App() {
-    return <div className="App"></div>;
+    return (
+        <>
+            <Route exact={true} path="/" component={LandingPage} />
+            <Route exact={true} path="/login" component={LoginPage} />
+            <Route exact={true} path="/register" component={Register} />
+        </>
+    );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
+import { HashRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Router>
         <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+    </Router>,
+    document.getElementById("render")
 );
